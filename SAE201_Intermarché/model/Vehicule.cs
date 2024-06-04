2c198456c96ae243ab532ce5eaa4b120dd612dbe
Ewan-Ponchaud <155959234+Ewan-Ponchaud@users.noreply.github.com>
2024-06-04 14:38:19 +0200

    créé classe vehicule et enum categorieVehicule

---
+++ SAE201_Intermarché/model/Vehicule.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAE201_Intermarche.model
+{
+
+    enum categorieVehicule
+    {
+        COMPACT, ECONOMIQUE, FAMILIALE, HYBRIDE, CAMION_BENNE
+    }
+    public class Vehicule
+    {
+
+
+
+
+
+    }
+}
