29060a853b9c1d38941c49154ae1fddc2d99f84a
Justin Bonansea <justin.bonansea@etu.univ-smb.fr>
2024-06-04 14:48:10 +0200

    renommage des classes

--- SAE201_Intermarché/model/EntiteEmploye.cs
+++ SAE201_Intermarché/model/EntiteEmploye.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace SAE201_Intermarche.model
 {
-    internal class Employe
+    internal class EntiteEmploye
     {
     }
 }
--- SAE201_Intermarché/model/EntiteMagasin.cs
+++ SAE201_Intermarché/model/EntiteMagasin.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace SAE201_Intermarche.model
 {
-    internal class Magasin
+    internal class EntiteMagasin
     {
     }
 }
