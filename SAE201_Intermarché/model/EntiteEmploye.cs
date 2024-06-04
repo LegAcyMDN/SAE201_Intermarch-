4bd4c427f856f0764eaa3ce52edeb529e5c8f375
Justin Bonansea <justin.bonansea@etu.univ-smb.fr>
2024-06-04 15:04:01 +0200

    ajout de données dans employé, magasin, assurance

--- SAE201_Intermarché/model/EntiteEmploye.cs
+++ SAE201_Intermarché/model/EntiteEmploye.cs
@@ -8,5 +8,37 @@ namespace SAE201_Intermarche.model
 {
     internal class EntiteEmploye
     {
-    }
+		private int numEmploye;
+
+		public int NumEmploye
+		{
+			get { return numEmploye; }
+			set { numEmploye = value; }
+		}
+
+		private int numMagasin;
+
+		public int NumMagasin
+		{
+			get { return numMagasin; }
+			set { numMagasin = value; }
+		}
+
+		private String login;
+
+		public String Login
+		{
+			get { return login; }
+			set { login = value; }
+		}
+
+		private String mdp;
+
+		public String Mdp
+		{
+			get { return mdp; }
+			set { mdp = value; }
+		}
+
+	}
 }
