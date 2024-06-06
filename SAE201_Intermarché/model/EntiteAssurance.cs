f1a61c38f98aa7a86b6ce0ced84834ef1256e0c4
LegAcyMDN <121859179+LegAcyMDN@users.noreply.github.com>
2024-06-06 11:50:56 +0200

    modif resà, assurance, detail

--- SAE201_Intermarché/model/EntiteAssurance.cs
+++ SAE201_Intermarché/model/EntiteAssurance.cs
@@ -6,7 +6,37 @@ using System.Threading.Tasks;
 
 namespace SAE201_Intermarche.model
 {
-    internal class EntiteAssurance
+    public class EntiteAssurance
     {
+		private int numAssurance;
+
+		public int NumAssurance
+		{
+			get { return numAssurance; }
+			set { numAssurance = value; }
+		}
+
+		private String descriptionAssurance;
+
+		public String DescriptionAssurance
+		{
+			get { return descriptionAssurance; }
+			set { descriptionAssurance = value; }
+		}
+
+		private int prixAssurance;
+
+		public int PrixAssurance
+		{
+			get { return prixAssurance; }
+			set { prixAssurance = value; }
+		}
+
+        public EntiteAssurance(int numAssurance, string descriptionAssurance, int prixAssurance)
+        {
+            NumAssurance = numAssurance;
+            DescriptionAssurance = descriptionAssurance;
+            PrixAssurance = prixAssurance;
+        }
     }
 }
